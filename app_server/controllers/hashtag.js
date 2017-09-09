456022b5334a3918bd735692fa463e73a742d11d
Paulo Coelho <paulo.coe07@gmail.com>
2017-09-09 19:33:45 -0300

    Add mongoose connection and error handling and new actions

--- app_server/controllers/hashtag.js
+++ app_server/controllers/hashtag.js
@@ -1,8 +1,38 @@
 'use strict'
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+var hashtags = require('../models/hashtag')
+var config = require('../../config/config')
 
+// Connect to database
+mongoose.connect(config.db.uri)
 
-module.exports.index = function(req, res){
-   res.render('index')
+// On connection success
+mongoose.connection.on('connected', function () {
+  console.log('Connected to database: ', config.db.uri)
+})
+
+// On connection error
+mongoose.connection.on('error', function (err) {
+  console.log('Database error', err)
+})
+
+// Index page action
+module.exports.index = function (req, res) {
+  if (!req.cookies['access_token']) { res.render('index') } else { res.redirect('/home') }
+}
+
+// Home page action with the list of stored hashtags
+module.exports.homelist = function (req, res) {
+  if (!req.cookies['access_token']) { res.redirect('/') }
+  hashtags.find({}).lean().exec()
+}
+
+// Delete Action
+module.exports.delete = function (req, res) {
+  if (!req.cookies['access_token']) { res.redirect('/') }
+  var hashtagName = req.body.name
+  hashtags.findOneAndUpdate({ name: hashtagName })
+// After deletion, redirects the user to home screen
+  res.redirect('/home')
 }
